750016fc4dc7870de45020eac83c598274465641
Ulisse Mini <ulisse.mini@gmail.com>
2024-03-31 18:11:37 -0700

    Auto-create threads

--- main.py
+++ main.py
@@ -3,25 +3,41 @@ import dotenv
 import os
 
 dotenv.load_dotenv()
-TOKEN = os.environ['DISCORD_TOKEN']
+
+TOKEN = os.environ['DISCORD_BOT_TOKEN']
+GUILD_ID = os.environ['GUILD_ID']
+THREAD_ONLY_CATEGORY_ID = os.environ['THREAD_ONLY_CATEGORY_ID']
+
+bot = discord.Bot(intents=discord.Intents.all())
 
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+
+@bot.event
+async def on_message(message: discord.Message):
+    if message.author == bot.user:
         return
 
+    print(f'[{message.author}]: {message.content}')
     if message.content.startswith('.hello'):
         await message.channel.send('Hello!')
 
+    # create a thread if it's a thread only channel!
+    if isinstance(message.channel, discord.TextChannel):
+        if message.channel.category_id == THREAD_ONLY_CATEGORY_ID:
+            name = message.content[:50]
+            thread = await message.create_thread(name=name, auto_archive_duration=60)
+            await thread.send(f"Thread created (thread only channel)")
 
-client.run(TOKEN)
+
+
+@bot.slash_command(guild_ids=[GUILD_ID])
+async def hello(ctx):
+    await ctx.respond("Hello!")
+
+
+
+bot.run(TOKEN)
